2eb0ccd12dc4f41ea68f4d60143a5649c5195bd5
Jakub Kanclerz <jakub.kanclerz@gmail.com>
2021-01-09 14:04:15 +0100

    introduce basket specification

---
+++ src/main/java/pl/jkanclerz/vouchershop/sales/Basket.java
@@ -0,0 +1,36 @@
+package pl.jkanclerz.vouchershop.sales;
+
+import pl.jkanclerz.vouchershop.catalog.Product;
+
+import java.util.Collections;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
+public class Basket {
+    private final Map<String, Product> products;
+
+    public Basket() {
+        this.products = new HashMap<>();
+    }
+
+    public boolean isEmpty() {
+        return products.isEmpty();
+    }
+
+    public void add(Product product) {
+        products.put(product.getId(), product);
+    }
+
+    public Integer getProductsCount() {
+        return products.size();
+    }
+
+    public List<BasketItem> getBasketItems() {
+        return Collections.emptyList();
+    }
+
+    public void remove(String id) {
+
+    }
+}
---
+++ src/main/java/pl/jkanclerz/vouchershop/sales/BasketItem.java
@@ -0,0 +1,11 @@
+package pl.jkanclerz.vouchershop.sales;
+
+public class BasketItem {
+    public String getProductId() {
+        return null;
+    }
+
+    public int getQuantity() {
+        return 0;
+    }
+}
