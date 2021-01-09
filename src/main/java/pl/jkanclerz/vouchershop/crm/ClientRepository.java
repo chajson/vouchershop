bfd59a57660fda9810d623ae91154bc39848085f
Jakub Kanclerz <jakub.kanclerz@gmail.com>
2021-01-09 12:35:34 +0100

    introduce crm module via jpa

---
+++ src/main/java/pl/jkanclerz/vouchershop/crm/ClientRepository.java
@@ -0,0 +1,8 @@
+package pl.jkanclerz.vouchershop.crm;
+
+import org.springframework.data.repository.CrudRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface ClientRepository extends CrudRepository<Client, Integer> {
+}
