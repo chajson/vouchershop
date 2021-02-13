6acc29f83b6721fc0ae2bf90a30aff795b0e5e9c
Piotr Chaja <piotr.chaja@miquido.com>
2021-02-13 15:00:03 +0100

    feat/ added vouchershop

--- src/main/java/pl/pchaja/vouchershop/App.java
+++ src/main/java/pl/pchaja/vouchershop/App.java
@@ -1,4 +1,4 @@
-package pl.jkanclerz.vouchershop;
+package pl.pchaja.vouchershop;
 
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.SpringApplication;
